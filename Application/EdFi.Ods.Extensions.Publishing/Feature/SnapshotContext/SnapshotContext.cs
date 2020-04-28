bc71dc1a36715c74169352e9929a37d9ef74afa1
Geoff McElhanon <geoffrey@mcelhanon.com>
2020-04-28 14:55:39 -0500

    Publishing Extension Bug Fix - Return null from snapshot context provider rather than an "Empty" instance, which was leading to logic handling failures.

--- Application/EdFi.Ods.Extensions.Publishing/Feature/SnapshotContext/SnapshotContext.cs
+++ Application/EdFi.Ods.Extensions.Publishing/Feature/SnapshotContext/SnapshotContext.cs
@@ -2,13 +2,11 @@ namespace EdFi.Ods.Extensions.Publishing.Feature.SnapshotContext
 {
     public class SnapshotContext
     {
-        public SnapshotContext(string snapshotIdentifer)
+        public SnapshotContext(string snapshotIdentifier)
         {
-            SnapshotIdentifer = snapshotIdentifer;
+            SnapshotIdentifier = snapshotIdentifier;
         }
 
-        public string SnapshotIdentifer { get; }
-
-        public static SnapshotContext Empty { get; } = new SnapshotContext(null);
+        public string SnapshotIdentifier { get; }
     }
 }
