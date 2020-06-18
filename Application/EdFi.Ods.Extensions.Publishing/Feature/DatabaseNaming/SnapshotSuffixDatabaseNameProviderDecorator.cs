22dcb0256fcfff008958b67ffeefce41a23bc8ac
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2020-06-18 02:08:58 -0500

    Adjustments for ODS database name provider changes in the API in v3.4.0.

--- Application/EdFi.Ods.Extensions.Publishing/Feature/DatabaseNaming/SnapshotSuffixDatabaseNameProviderDecorator.cs
+++ Application/EdFi.Ods.Extensions.Publishing/Feature/DatabaseNaming/SnapshotSuffixDatabaseNameProviderDecorator.cs
@@ -11,13 +11,13 @@ namespace EdFi.Ods.Extensions.Publishing.Feature.DatabaseNaming
     /// to allow API requests to be serviced off a static snapshot of the ODS for the purposes
     /// of maintaining a fully isolated context for client API publishing using Change Queries.
     /// </summary>
-    public class SnapshotSuffixDatabaseNameProviderDecorator : IDatabaseNameProvider
+    public class SnapshotSuffixDatabaseNameReplacementTokenProvider : IDatabaseNameReplacementTokenProvider
     {
-        private readonly IDatabaseNameProvider _next;
+        private readonly IDatabaseNameReplacementTokenProvider _next;
         private readonly ISnapshotContextProvider _snapshotContextProvider;
 
-        public SnapshotSuffixDatabaseNameProviderDecorator(
-            IDatabaseNameProvider next,
+        public SnapshotSuffixDatabaseNameReplacementTokenProvider(
+            IDatabaseNameReplacementTokenProvider next,
             ISnapshotContextProvider snapshotContextProvider)
         {
             _next = next;
@@ -25,17 +25,17 @@ namespace EdFi.Ods.Extensions.Publishing.Feature.DatabaseNaming
         }
 
         /// <summary>
-        /// Gets the database name with a snapshot-specific suffix appended.
+        /// Gets the replacement token with a snapshot-specific suffix appended.
         /// </summary>
-        /// <returns>The snapshot database name.</returns>
+        /// <returns>The configured token replacement with a snapshot database suffix appended.</returns>
         /// <exception cref="FormatException">Occurs if the snapshot identifier in context
         /// is not a 32-character alphanumeric value.</exception>
-        public string GetDatabaseName()
+        public string GetReplacementToken()
         {
             var snapshotContext = _snapshotContextProvider.GetSnapshotContext();
             
             if (snapshotContext == null || string.IsNullOrEmpty(snapshotContext.SnapshotIdentifier))
-                return _next.GetDatabaseName();
+                return _next.GetReplacementToken();
 
             // To prevent possible tampering, snapshot identifier must be a 32 character long value (accommodates a GUID)
             if (snapshotContext.SnapshotIdentifier.Length != 32)
@@ -45,7 +45,7 @@ namespace EdFi.Ods.Extensions.Publishing.Feature.DatabaseNaming
             if (snapshotContext.SnapshotIdentifier.Any(c => !(char.IsLetter(c) || char.IsDigit(c))))
                 throw new FormatException("Invalid value for snapshot identifier.");
 
-            return _next.GetDatabaseName() + $"_SS{snapshotContext.SnapshotIdentifier}";
+            return _next.GetReplacementToken() + $"_SS{snapshotContext.SnapshotIdentifier}";
         }
     }
 }
